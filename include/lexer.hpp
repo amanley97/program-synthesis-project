53eb657b7270bd4f866f1dc909b3d49f03a12d0a
Harlan Williams <hrw@ku.edu>
2024-11-07 10:27:26 -0600

    add docs

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -15,6 +15,9 @@
 
 using TokenPair = std::pair<std::string, Token>;
 
+/**
+ * \class Lexeme
+ */
 class Lexeme {
 public:
     Lexeme(const char *symbol, Token token, int priority)
@@ -31,9 +34,24 @@ private:
 
 /**
  * \class Lexer
+ * 
+ * \brief Handles converting source strings into lexems, or tokens, which can then be used to parse the
+ * given expression at a higher level than single characters.
  */
 class Lexer {
 public:
+    /**
+     * \brief Add a new lexeme to the Lexer. This associates a symbol (as a regular expression) to a Token,
+     * and the Lexer will attempt to match this symbol in any input stream it is given. The Lexer will
+     * favor the longest matching symbol, which gives multi-character tokens priority over single-character,
+     * tokens, *e.g.*, `->` will be favored over `-` wherever it appears.
+     * 
+     * \param symbol Regular expression to match on
+     * \param token Token to push to the token stream on match
+     * \param priority An optional priority, to force the Lexer to favor this symbol, token pair over others
+     * with lower priority in case the matched length is the same. This is useful for differentiating keywords
+     * from identifiers, for example.
+     */
     void add_lexeme(const char *symbol, Token token, int priority = 0) noexcept;
 
     /**
