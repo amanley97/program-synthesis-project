3d1f2378aaec07b2d80cf937d33ec994207a0c66
Harlan Williams <hrw@ku.edu>
2024-11-14 00:41:10 -0600

    add z3 libraries for linux and add small example

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 CPP := g++ -std=c++17
 OPTFLAGS := -O0
 CPPFILES := $(wildcard src/*.cpp)
+Z3-BIN := thirdparty/z3-4.13.3-x64-glibc-2.35/bin
+Z3-INC := thirdparty/z3-4.13.3-x64-glibc-2.35/include
 
 synthesizer: $(CPPFILES)
-	$(CPP) -Wall $(OPTFLAGS) -I./include $^ -o $@
+	$(CPP) -Wall $(OPTFLAGS) -I./include -I$(Z3-INC) $^ $(Z3-BIN)/libz3.a -o $@
 
 lexer_test: src/lexer.cpp src/regex.cpp tests/lexer_test.cpp
 	$(CPP) -fprofile-arcs -ftest-coverage -O0 -I./include $^ -o $@
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 
+#include <z3++.h>
+
 #include "regex.hpp"
 #include "tokens.hpp"
 #include "lexer.hpp"
@@ -18,7 +20,31 @@ auto src = "comp Add<G>(); comp Mul<G>();\n"
            "    a0 := A<G+n>(l, r);\n"
            "}\n";
 
+void demorgan() {
+    using namespace z3;
+    std::cout << "de-Morgan example\n";
+    
+    context c;
+
+    expr x = c.bool_const("x");
+    expr y = c.bool_const("y");
+    expr conjecture = (!(x && y)) == (!x || !y);
+    
+    solver s(c);
+    // adding the negation of the conjecture as a constraint.
+    s.add(!conjecture);
+    std::cout << s << "\n";
+    std::cout << s.to_smt2() << "\n";
+    switch (s.check()) {
+        case unsat:   std::cout << "de-Morgan is valid\n"; break;
+        case sat:     std::cout << "de-Morgan is not valid\n"; break;
+        case unknown: std::cout << "unknown\n"; break;
+    }
+}
+
 int main(int argc, char *argv[]) {
+    demorgan();
+
     Lexer lexer;
     lexer.add_lexeme("@", Token::At);
     lexer.add_lexeme(":", Token::Colon);
