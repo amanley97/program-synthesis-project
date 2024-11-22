3dd05853b35d9fc47b79003d2355a97a596ec093
Harlan Williams <harlanrhwilliams@gmail.com>
2024-11-22 15:48:06 -0600

    modify function sig

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     expr = pyf.parse("""
     (comp AddMult
       ((event G 2))
-      ((interface go G 1) (in-port a (range G (+ G 1)) 32) (in-port b (range G (+ G 1)) 32) (out-port out (range (+ G 2) (+ G 3))))
+      ((interface go G 1) (in-port a (range G (+ G L)) 32) (in-port b (range G (+ G 1)) 32) (out-port out (range (+ G 2) (+ G 3))))
       ())
     """)
 
