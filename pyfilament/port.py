3a1ff9f39b1e2b0afb003fb6a46ad35b8c33cfb7
Alex Manley <amanley97@ku.edu>
2024-12-04 20:17:52 -0600

    port class

--- pyfilament/port.py
+++ pyfilament/port.py
@@ -1,2 +1,12 @@
+from typing import Optional
+
 class Port:
-    pass
+    def __init__(self, name: str, direction: str, range_: Optional[str], width: int):
+        self.name = name
+        self.direction = direction
+        self.range_ = range_
+        self.width = width
+
+    def __repr__(self):
+        return (f"Port(name={self.name}, direction={self.direction}, "
+                f"range_={self.range_}, width={self.width})")
