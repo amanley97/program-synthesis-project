f423af2dde61b7e2e54b49c2831e076dc71bf566
Alex Manley <amanley97@ku.edu>
2024-12-11 15:36:35 -0600

    clean main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# from z3 import *
 import pyfilament as pyf
 from argparse import ArgumentParser
 
@@ -18,48 +17,3 @@ if __name__ == '__main__':
 
     lower_fil = pyf.generate_lower(comp)
     print(f"Lower Filament Form: \n-----\n{lower_fil}\n")
-
-"""
-comp main<G: 1>(
-    @interface[G] go: 1,
-    @[G, G+1] left: 32,
-    @[G, G+1] right: 32,
-    @[G+1, G+2] opt: 32,
-) -> (@[G+3, G+4] out: 32) {
- 
-    // AND block
-    A := new And[32];
-    a0 := A<G>(left, right);
-    and_stage := new Register[32]<G, G+2>(a0.out); // Register AND result
- 
-    // XOR block
-    X := new Xor[32];
-    x0 := X<G+1>(and_stage.out, opt);
-    xor_stage := new Register[32]<G+1, G+3>(x0.out); // Register XOR result
- 
-    // Output register to satisfy timing
-    r0 := new Register[32]<G+2, G+4>(xor_stage.out);
-    out = r0.out;
-}
-"""
-
-"""
-(comp main
-  (events
-   (event G))
-  (ports
-   (interface[1] (G) go)  # G is not phantom in this component because it has an interface
-   (in-port[32]  (G       (+ G 1)) left)
-   (in-port[32]  (G       (+ G 1)) right)
-   (in-port[32]  ((+ G 1) (+ G 2)) opt)
-   (out-port[32] ((+ G 3) (+ G 4)) out))
-  (instantiations
-   (A (new And[32]))
-   (X (new Xor[32])))
-  (invocations
-   (a0 (A (G) left right))
-   (x0 (X (+ G 1) a0 opt)))
-  (connect (out x0))
-# (constraints (> L G)) # optional constraints over events, if there are multiple events
-)
-"""
